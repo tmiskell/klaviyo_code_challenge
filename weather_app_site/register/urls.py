826eeb4fcad7f2b343b6213c80d631fc7a4fb8a5
Timothy L Miskell <timothymiskell@gmail.com>
2016-12-25 21:46:55 -0500

    Fixed wiring between register and confirmation page. Included simple error messages after validating email addresses.

--- weather_app_site/register/urls.py
+++ weather_app_site/register/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'register'
 urlpatterns = [ url( r"^$", views.index, name="index" ),
-                url( r"^/confirmation/$", views.index, name="confirmation" ),
+                url( r"^confirmation/$", views.confirmation, name="confirmation" ),
               ]
--- weather_app_site/register/views.py
+++ weather_app_site/register/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Address, Location
 from .global_vars import NUM_CITIES, TITLE
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 def index( request ):
@@ -18,14 +19,28 @@ def confirmation( request ):
     template = 'register/confirmation.html'
     result = "Successfully registered e-mail address"
     try:
-        a = Address( email_address=request.POST['email_address'], create_date=timezone.now() )
-        # Ensure that e-mail is validated before saving.
+        try:
+            pk = int( request.POST.get('next_city_pk', None) )
+        except TypeError:
+            raise ValidationError( "Invalid primary key supplied" )
+        fk = Location.objects.all().filter( pk=pk )
+        if len(fk) != 1:
+            # Primary key supplied should be valid, and should be a unique ID.
+            raise ValidationError("Server side error")
+        email_address = request.POST.get( 'email_address', '')
+        a = Address( location=fk[0], email_address=email_address, creation_date=timezone.now() )
+        # Validate e-mail before saving.
         a.full_clean()
         a.save()
-    except (django.db.IntegrityError):
-        result = "E-mail address has already been registered"
-    except (ValidationError):
-        result = "Invalid e-mail address"
+    except ValidationError, exc:
+        # Email was invalid or not unique. Capture the error message.
+        if isinstance( exc, dict ):
+            result = ""
+            for key, value in exc.items():
+                result += value + " "
+            result = result.rstrip()
+        else:
+            result = "%s" % exc
     context = { 'title': TITLE,
                 'result': result,
                 'email_address': email_address
